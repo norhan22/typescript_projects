8a4e864a15eeb2007d8a6fc5ad2e65d9ace54c73
Norhan Mohammed <norhan@sulfah.com>
2023-10-04 16:29:22 +0300

    init project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# typescript_projects
+# Typescript Scripts
+
+## [Notes](https://norhan-mohammed.notion.site/Typescript-dfb3e1501f6647649f92497b25cabfee?pvs=4)
--- variables.ts
+++ variables.ts
@@ -1,17 +1,52 @@
 let text: string = 'test ';
 let num: number = 444;
-let anyVal: any = 888;
-anyVal = true
+let anyType: any = 888;
 
+// output
+console.groupCollapsed('Static Types')
+console.log('text', text) // test
+console.log('num', num) //  444
+console.log('text + num', text + num) // test 444
+console.log('before:anyType', anyType) // true
+anyType = true
+console.log('after:anyType', anyType) // true
+console.groupEnd()
+
+//////////////////////////////////////////////
 enum numbers {
     num1 = 5, num2 = 2
 }
 
+console.groupCollapsed('Enum Ex')
+console.log('numbers', numbers)
+console.groupEnd()
+//////////////////////////////////////////////////
 const args = {a: 1, b: 2} as const;
-args.a;
-args.b;
 
-console.log(text + num)
-console.log(anyVal)
-console.log(numbers)
-console.log(args)
+console.groupCollapsed('as const')
+console.log('args', args)
+args.a = 5
+console.log('after: args.a', args.a)
+console.log('after args', args)
+console.groupEnd()
+/////////////////////////////////////////////////
+type arr1 = [number, string]
+const arr1Ex: arr1 = [5, 'test']
+
+// output
+console.groupCollapsed('type (custom types)')
+console.log('type: arr1Ex', arr1Ex)
+console.groupEnd()
+/////////////////////////////////////////////////
+
+interface arr2 = {a}
+const arr2Ex: arr1 = [5, 'test']
+
+// output
+console.groupCollapsed('type (custom types)')
+console.log('type: arr1Ex', arr1Ex)
+console.groupEnd()
+/////////////////////////////////////////////////
+
+
+
